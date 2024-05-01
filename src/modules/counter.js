83339a1f335d25c3903e50e8fa04c24ffe389487
김지호 <jiho001108@gmail.com>
2024-05-01 12:14:52 +0900

    createAction 직렬화 문제 해결

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,13 +1,14 @@
 // counter 리덕스 모듈
-import { createAction, handleActions } from "redux-actions";
+import { handleActions } from "redux-actions";
 
 // 액션 정의
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 // 액션 생성 함수 정의
-export const increase = createAction(INCREASE);
-export const decrease = createAction(DECREASE);
+// export const increase = createAction(INCREASE); // 직렬화 문제 발생함, redux-actions
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
 
 // 초기 상태 정의
 const initialState = 0; // 상태는 객체일 필요 없다
