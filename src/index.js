117d2e8c8db2f606a15e24d6cbfc5694e06a381d
김지호 <jiho001108@gmail.com>
2024-05-01 16:31:46 +0900

    redux-logger 미들웨어 적용

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
 import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
+const logger = createLogger();
 const store = configureStore({
 	reducer: rootReducer,
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(loggerMiddleware),
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 	devTools: process.env.NODE_ENV !== "production", // 개발 환경에서만 DevTools 활성화
 });
 
