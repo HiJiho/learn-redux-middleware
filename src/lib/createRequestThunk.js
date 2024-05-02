3d374f24928ce112e38cd1199221881ef3e9a538
김지호 <jiho001108@gmail.com>
2024-05-02 16:43:17 +0900

    루트 리듀서에 loading 리듀서 추가 및 createRequestThunk에서 loading 액션 생성 함수 사용

--- src/lib/createRequestThunk.js
+++ src/lib/createRequestThunk.js
@@ -1,6 +1,8 @@
 // api 요청하는 thunk 함수를 생성하는 함수
 // 기존 thunk 함수 리팩토링
 
+import { finishLoading, startLoading } from "../modules/loading";
+
 export default function createRequestThunk(type, request) {
 	// 성공 및 실패 액션 타입 정의
 	const SUCCESS = `${type}_SUCCESS`;
@@ -8,18 +10,21 @@ export default function createRequestThunk(type, request) {
 
 	return (params) => async (dispatch) => {
 		dispatch({ type }); // 요청 시작
+		dispatch(startLoading(type));
 		try {
 			const response = await request(params);
 			dispatch({
 				type: SUCCESS,
 				payload: response.data,
 			}); // 요청 성공
+			dispatch(finishLoading(type));
 		} catch (e) {
 			dispatch({
 				type: FAILURE,
 				payload: e,
 				error: true,
 			}); // 에러 발생
+			dispatch(finishLoading(type));
 			throw e;
 		}
 	};
--- src/modules/loading.js
+++ src/modules/loading.js
@@ -5,13 +5,12 @@ import { createAction, handleActions } from "redux-actions";
 const START_LOADING = "loading/START_LOADING";
 const FINISH_LOADING = "loading/FINISH_LOADING";
 
+// 액션 생성 함수
 // 요청을 위한 액션 타입을 payload로 설정(예: "sample/GET_POST")
-
 export const startLoading = createAction(
 	START_LOADING,
 	(requestType) => requestType
 );
-
 export const finishLoading = createAction(
 	FINISH_LOADING,
 	(requestType) => requestType
