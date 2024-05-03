1e582958104ed2e23ffac9bdccfa5da4ac4190d1
김지호 <jiho001108@gmail.com>
2024-05-03 10:43:08 +0900

    Add createRequestSaga function and refactor sample module

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -5,62 +5,21 @@
 import { createAction, handleActions } from "redux-actions";
 import { call, put, takeLatest } from "redux-saga/effects";
 import * as api from "../lib/api";
-import { finishLoading, startLoading } from "./loading";
+import createRequestSaga from "../lib/createRequestSaga";
 
 // 액션 타입 선언
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
-const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
 
 const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
-const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 export const getPost = createAction(GET_POST, (id) => id);
 export const getUsers = createAction(GET_USERS);
 
-// getPost가 호출될 때 실행 할 saga
-function* getPostSaga(action) {
-	// action.payload: 전달된 id 값
-	yield put(startLoading(GET_POST)); // 로딩 시작
-	// 파라미터로 action을 받아 오면 액션의 정보를 조회 가능
-	try {
-		// call을 사용하면 Promise를 반환하는 함수를 호출하고, 기다릴 수 있다
-		// 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수
-		const post = yield call(api.getPost, action.payload); // api.getPost(action.payload) 의미
-		yield put({
-			type: GET_POST_SUCCESS, // GET_POST_SUCCESS 액션을 디스패치
-			payload: post.data, // action.payload = post.data(-> api.getPost 요청 결과)
-		});
-	} catch (e) {
-		// try-catch 문을 사용해 에러도 잡을 수 있다
-		yield put({
-			type: GET_POST_FAILURE,
-			payload: e,
-			error: true,
-		});
-	}
-	yield put(finishLoading(GET_POST)); // 로딩 완료
-}
-
-// getUsers가 호출될 때 실행할 saga
-function* getUsersSaga() {
-	yield put(startLoading(GET_USERS));
-	try {
-		const users = yield call(api.getUsers);
-		yield put({
-			type: GET_USERS_SUCCESS,
-			payload: users.data,
-		});
-	} catch (e) {
-		yield put({
-			type: GET_USERS_FAILURE,
-			payload: e,
-			error: true,
-		});
-		yield put(finishLoading(GET_USERS));
-	}
-}
+// getPost, getUsers가 호출될 때 실행할 saga
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
 // 사가 리스너, 사가 미들웨어에 의해 모니터링 중
 // 모니터링 되는 액션 타입: GET_POST, GET_USERS
